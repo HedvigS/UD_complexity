9eb2252530c84a3e2587e22d8ec0348ad572a97c
HedvigS <hedvig.skirgard@gmail.com>
2024-01-31 17:43:21 +0100

    Create 01_requirements.R

---
+++ code/01_requirements.R
@@ -0,0 +1,41 @@
+### loading packages
+
+groundhog_date = "2024-01-15"
+
+if(!("groundhog" %in% rownames(installed.packages()))){
+  
+  install.packages("groundhog")
+  library(groundhog)
+  
+}else{
+  
+  library(groundhog)
+  
+}
+
+groundhog_dir <- paste0("groundhog_libraries_", groundhog_date)
+
+if(!dir.exists(groundhog_dir)){
+  dir.create(groundhog_dir)
+}
+
+groundhog::set.groundhog.folder(groundhog_dir)
+
+pkgs <- c("dplyr",
+          "readr",
+          "tidyr",
+          "stringr",
+          "ggplot2",
+          "udpipe",
+          "reshape2",
+          "tibble"
+)
+
+groundhog.library(pkgs, groundhog_date)
+
+# UD data
+#https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-5287
+#https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-5287/ud-treebanks-v2.13.tgz?sequence=1&isAllowed=y
+
+
+
