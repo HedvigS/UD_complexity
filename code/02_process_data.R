d2ec88380d5b3e34316b0052b59487b4d5d7fa20
HedvigS <hedvig.skirgard@gmail.com>
2024-02-03 20:06:36 +0100

    Update 02_process_data.R

--- code/02_process_data.R
+++ code/02_process_data.R
@@ -5,13 +5,14 @@ fns <- list.files(path = "../data/ud-treebanks-v2.13/",
                   full.names = T, 
                   recursive = T)
 
-n <- length(fns)
 
-for(i in 1:n){
-fn <- fns[i]
+for(i in 1:length(fns)){
+# i <- 1
+  fn <- fns[i]
+
 
   cat(paste0("I'm on ", fn, ".\n"))
-  cat(paste0("It is number ", i, " out of ", n ,".\n"))
+  cat(paste0("It is number ", i, " out of ", length(fns) ,".\n"))
   
 conllu <- udpipe::udpipe_read_conllu(fn)
 
@@ -35,8 +36,25 @@ df_spec <- conllu_split %>%
   mutate(fn = fn)
 
     fn_spec <- paste0("output/sum_dfs/", basename(fn), "_sum_df.tsv")
-    df_spec %>% 
+
+        df_spec %>% 
       write_tsv(file = fn_spec)
-    
+
+token_mean <- df_spec %>% 
+      group_by(fn) %>% 
+      summarise(n_feats_per_token_mean = mean(n_feats_per_token), .groups = "drop")
+
+sent_mean <- df_spec %>% 
+  distinct(sentence_id, fn,n_feats_per_sent) %>% 
+  group_by(fn) %>% 
+  summarise(n_feats_per_semt_mean = mean(n_feats_per_sent), .groups = "drop")
+
+fn_spec <- paste0("output/sum_dfs/", basename(fn), "_sum_df_means.tsv")
+
+
+sent_mean %>% 
+  full_join(token_mean, by = "fn") %>% 
+  write_tsv(file = fn_spec)        
 }
+
 
