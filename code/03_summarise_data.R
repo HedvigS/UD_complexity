9fd71c00b08f6cf25c3821ec07fcd2f04a8296b0
HedvigS <hedvig.skirgard@gmail.com>
2024-02-04 13:40:27 +0100

    Update 03_summarise_data.R

--- code/03_summarise_data.R
+++ code/03_summarise_data.R
@@ -1,5 +1,22 @@
 source("01_requirements.R")
 
+fns <- list.files("output/sum_dfs/", pattern = "df_means.tsv", full.names = T)
+
+df <- data.frame("fn" = as.character(),
+                 "n_feats_per_semt_mean" =  as.character(),
+                 "n_feats_per_token_mean" =  as.character())
+
+
+for(fn in fns){
+ df <- read_tsv(fn, col_types = cols(.default = "c")) %>% 
+   full_join(df)
+  
+}
+
+df$n_feats_per_token_mean %>% as.numeric() %>% hist()  
+  
+
+
 df <- read_tsv(file = "output/all_conllu_df.tsv")
 
 
