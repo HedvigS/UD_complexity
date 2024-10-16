4cc1ac4871341dbc34bea22278d49c905932dcc0
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-10-16 12:10:19 +0200

    Create 03_google_pop.R

---
+++ code/03_google_pop.R
@@ -0,0 +1,20 @@
+source("01_requirements.R")
+source("basemap.R")
+
+google_pop_stats <- read_tsv("https://raw.githubusercontent.com/google-research/url-nlp/refs/heads/main/linguameta/linguameta.tsv", show_col_types = F) %>% 
+  dplyr::select(glottocode, Pop = estimated_number_of_speakers) %>% 
+  filter(!is.na(glottocode)) %>% 
+  filter(!is.na(Pop))
+  
+google_pop_stats %>%  
+  write_tsv("../data/google_pop.tsv")
+
+df <- google_pop_stats %>% 
+  left_join(Glottolog, by = "glottocode")
+
+basemap +
+  geom_jitter(data = df, mapping = aes(x = Longitude, y = Latitude, size = Pop, color = log10(Pop))) +
+#  theme(plot.margin = unit(c(0,0,0,0), "cm")) +
+  ggtitle("Speaker population per language from Google's LinguaMeta-project \n(github.com/google-research/url-nlp)")
+
+ggsave("output/plots/pop_map.png", height = 10, width = 12)
