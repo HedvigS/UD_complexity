25d7ff35dea1ec4ac2f592787ede13bfac031623
HedvigS <hedvig.skirgard@gmail.com>
2024-10-16 12:09:08 +0200

    Update 03_compare_UD_gb.R

--- code/03_compare_UD_gb.R
+++ code/03_compare_UD_gb.R
@@ -1,25 +1,18 @@
 source("01_requirements.R")
 
-UD_lgs <- read_tsv("../data/UD_languages.tsv", show_col_types = F)
-
-UD_complexity <- read_tsv("output/sum_dfs/all_sum_df_means.tsv", show_col_types = F) %>% 
-  mutate(conllu= basename(fn)) %>% 
-  left_join(UD_lgs, by = "conllu")
-
-UD_type_counts <- read_tsv("output/sum_dfs/all_sum_df_counts.tsv", show_col_types = F) %>% 
-  mutate(conllu= basename(fn)) %>% 
-  left_join(UD_lgs, by = "conllu") %>% 
-  dplyr::select(conllu, n_token, n_type, n_unique_lemma, n_sentence, n_feats, fn, glottocode)
-
+UD_complexity <- read_tsv("output/processed_data/df_means_feats_per_token_per_sentence.tsv", show_col_types = F) 
 
 gb_complexity <- read_tsv("output/processed_data/grambank_theo_scores.tsv", show_col_types = F) %>% 
   rename(glottocode = Language_ID)
 
-joined <- inner_join(UD_complexity, gb_complexity, by = "glottocode") %>% 
-  inner_join(UD_type_counts, by = join_by(fn, conllu, glottocode))
+pop <- read_tsv("../data/linguameta.tsv")
 
+joined <- UD_complexity %>% 
+  left_join(gb_complexity) %>% 
+  left_join(pop)
+  
 joined %>% 
-ggplot(aes(x = mean_morph, y = n_feats_per_token_mean)) +
+ggplot(aes(x = mean, y = Fusion)) +
   geom_point() +
   ggpubr::stat_cor(method = "pearson", p.digits = 2, geom = "label", color = "blue",
                    label.y.npc="top", label.x.npc = "left", alpha = 0.8) +
@@ -38,17 +31,12 @@ joined %>%
   geom_smooth(method='lm', formula = 'y ~ x') +
   theme_classic()
 
-png("output/plots/SPLOM_all.png", width = 30, height = 30, units = "cm", res = 300)
+png("output/plots/SPLOM.png", width = 15, height = 15, units = "cm", res = 300)
 joined %>% 
-  dplyr::select(n_feats_per_sent_mean,
-                n_feats_per_token_mean,
-                n_type,
-                n_token, 
-                n_feats,
-                n_sentence,
-                n_unique_lemma, 
-                mean_morph,
-                Informativity) %>% 
+  dplyr::select("UD_feats_per_token\n_per_sentence" = mean,
+                GB_Fusion = Fusion, 
+                GB_Informativity = Informativity, 
+                Google_pop = "estimated_number_of_speakers") %>% 
 pairs.panels(method = "pearson", # correlation method
              hist.col = "#a3afd1",# "#a9d1a3","",""),
              density = TRUE,  # show density plots
