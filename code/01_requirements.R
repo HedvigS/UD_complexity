43f6225fc69111234177c6f67f95db6e8fcc5fce
HedvigS <hedvig.skirgard@gmail.com>
2024-10-16 12:08:57 +0200

    Update 01_requirements.R

--- code/01_requirements.R
+++ code/01_requirements.R
@@ -1,33 +1,23 @@
-### loading packages
+### loading 
 
-groundhog_date <- "2024-01-17"
-
-if (!'groundhog' %in% installed.packages()) install.packages('groundhog')
-library('groundhog')
-
-groundhog_dir <- paste0("groundhog_libraries_", groundhog_date)
-
-groundhog::set.groundhog.folder(groundhog_dir)
-
-pkgs <- c("utf8", 
-          "Rcpp",
-          "plyr",
-          "data.table",
-        "dplyr",
-          "readr",
-          "tidyr",
-          "stringr",
-          "ggplot2",
-          "ggpubr",
-        "psych",
-          "udpipe",
-          "reshape2",
-          "tibble")
-
-groundhog.library(pkgs, groundhog_date)
-
-#source("fun_def_h_load.R")
-#h_load(pkg = pkgs)
+library(utf8) 
+library(Rcpp)
+library(plyr)
+library(data.table)
+library(dplyr)
+library(readr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+library(ggpubr)
+library(psych)
+library(udpipe)
+library(reshape2)
+library(tibble)
+library(purrr)
+library(viridis)
+library(forcats)
+library(ggridges)
 
 # UD data info
 #https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-5287
@@ -61,13 +51,13 @@ if(!dir.exists("output/UD_conllu/")){
 }
 
 
-if(!file.exists("../data/google_pop.tsv")){
+#if(!file.exists("../data/google_pop.tsv")){
 
-google_pop_stats <- read_tsv("https://github.com/google-research/url-nlp/raw/226e1a818aa1ce32311ef0931d9d44f2ab7ae084/language_metadata/data.tsv", show_col_types = F) %>% 
-  dplyr::select(Glottocode = `Glottocode (Glottolog)`, 
-                Pop = `Number of speakers (rounded)`)
-google_pop_stats %>% write_tsv("../data/google_pop.tsv", na = "")
-}
+#google_pop_stats <- read_tsv("https://github.com/google-research/url-nlp/raw/226e1a818aa1ce32311ef0931d9d44f2ab7ae084/language_metadata/data.tsv", show_col_types = F) %>% 
+#  dplyr::select(Glottocode = `Glottocode (Glottolog)`, 
+#                Pop = `Number of speakers (rounded)`)
+#google_pop_stats %>% write_tsv(../data/google_pop.tsv, na = )
+#}
 
 if(!dir.exists("output/plots")){
   dir.create("output/plots")
@@ -75,8 +65,8 @@ if(!dir.exists("output/plots")){
 
 
 UD_feats_df <- data.frame(
-  feat = c("PronType", "NumType", "Poss", "Reflex", "Abbr", "Typo", "Foreign", "ExtPos", "Gender", "Animacy", "NounClass", "Number", "Case", "Definite", "Deixis", "DeixisRef", "Degree", "VerbForm", "Mood", "Tense", "Aspect", "Voice", "Evident", "Polarity", "Person", "Polite", "Clusivity="),
-  type = c("Lexical", "Lexical", "Lexical", "Lexical", "Other","Other","Other","Other", "Nominal","Nominal","Nominal","Nominal","Nominal","Nominal","Nominal","Nominal","Nominal","Verbal","Verbal","Verbal","Verbal","Verbal","Verbal","Verbal","Verbal","Verbal","Verbal")
+  feat = c("PronType", "NumType", "Poss", "Reflex", "Abbr", "Typo", "Foreign", "ExtPos", "Gender", "Animacy", "NounClass", "Number", "Case", "Definite", "Deixis", "DeixisRef", "Degree", "VerbForm", "Mood", "Tense", "Aspect", "Voice", "Evident", "Polarity", "Person", "Polite", "Clusivity"),
+  type = c("Lexical", "Lexical",  "Lexical",  "Lexical",  "Other", "Other", "Other", "Other",  "Nominal", "Nominal", "Nominal", "Nominal", "Nominal", "Nominal", "Nominal", "Nominal", "Nominal", "Verbal", "Verbal", "Verbal", "Verbal", "Verbal", "Verbal", "Verbal", "Verbal", "Verbal", "Verbal")
   
 )
   
