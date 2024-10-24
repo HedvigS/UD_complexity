609b6a329b55e088210d341a4744a18743673c83
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-10-24 17:40:02 +0200

    Update 02_collapse_UD_dirs.R

--- code/02_collapse_UD_dirs.R
+++ code/02_collapse_UD_dirs.R
@@ -18,6 +18,8 @@ for(i in 1:nrow(UD_dirs)){
   
   #i <- 1
   
+  cat(paste0("I'm on ", UD_dirs[i,1], ". That's ", i, " of ", nrow(UD_dirs), ".\n"))
+  
   UD_dir_spec <- UD_dirs[i,] %>% 
     mutate(conllus = str_split(conllus, ";")) %>% 
     unnest(conllus)
@@ -45,10 +47,24 @@ for(i in 1:nrow(UD_dirs)){
 #      y = 1
      fn <-     paste0("../data/", UD_version, "/", UD_dir_spec[1,1],"/", UD_dir_spec[y,2] )
 
-    conllu <- udpipe::udpipe_read_conllu(fn) %>% 
-    dplyr::filter(!str_detect(token, "[[:punct:]]")|
-                    upos != "PUNCT")  %>% 
-      dplyr::mutate(across(everything(), as.character))
+    conllu <- udpipe::udpipe_read_conllu(fn) %>%   
+      dplyr::filter(!is.na(token)) %>% 
+      dplyr::mutate(lemma = ifelse(is.na(lemma), token, lemma)) %>% #if there isn't a lemma assigned, assume that the unique token is the lemma
+      #  dplyr::filter(!str_detect(feats, "Foreign=Yes|ExtPos=Yes")) %>% 
+      dplyr::filter(upos != "PUNCT")  %>% #remove tokens that are tagged with the part-of-speech tag punct for punctuation
+      dplyr::filter(token != "%") %>%  #remove tokens that are just "%"
+      dplyr::filter(token != "[:punct:]+") %>% #remove tokens that only consist of punctuation (including "$")
+      dplyr::filter(token != "[[punct]]+%") #remove tokens that consists of punctuation and percent sign only
+    
+    if(all(!is.na(conllu$doc_id))){
+      suppressWarnings( conllu$doc_id <- stringr::str_pad(as.numeric(conllu$doc_id), width = 3, pad = "0", side = "left") )
+    }
+    
+    suppressWarnings(conllu$paragraph_id <- stringr::str_pad(as.numeric(conllu$paragraph_id), width = 3, pad = "0", side = "left"))
+    suppressWarnings(conllu$token_id <- stringr::str_pad(as.numeric(conllu$token_id), width = 3, pad = "0", side = "left"))
+    
+    conllu <- conllu %>% 
+      tidyr::unite(doc_id, paragraph_id, sentence_id, token_id, col = "id", remove = F) 
   
     df <- df %>% 
       full_join(conllu, by = join_by(doc_id, paragraph_id, sentence_id, sentence, token_id, token, lemma, upos, xpos, feats, head_token_id,
