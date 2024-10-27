065b98b42255c15cedac479c20437784d6c27471
HedvigS <hedvig.skirgard@gmail.com>
2024-10-27 23:53:08 +0100

    Update 04_maps_counts.R

--- code/04_maps_counts.R
+++ code/04_maps_counts.R
@@ -4,45 +4,42 @@ Glottolog <- read_tsv("../data/glottolog_language_table_wide_df_3.0.tsv", show_c
   dplyr::select(glottocode = Glottocode, Longitude, Latitude, Family_ID, Language_level_ID, Name)
 
 UD_langs <- read_tsv("../data/UD_languages.tsv", show_col_types = F) %>% 
-  dplyr::select(dir, conllu, glottocode)
+  dplyr::select(dir,  glottocode)
 
 fns <- list.files("output/counts/", pattern = ".tsv", full.names = T)
 
 df_all <- data.frame(dir = as.character(), 
-                     sentences = as.numeric(),
+                     n_sentences = as.numeric(),
                      sum_tokens = as.numeric(), 
-                     mean_feats_per_token)
+                     mean_feats_per_token =  as.numeric())
 
 for(i in 1:length(fns)){
-  i <- 1
-  fn <- fns[1]
-  dir <- fn %>% basename() %>% str_replace_all(".tsv", "")
-  df <- read_tsv(fn)
+ # i <- 13
+  fn <- fns[i]
+  dir <- fn %>% basename() %>% str_replace_all(".tsv", "") %>% str_replace_all("counts_", "") %>% str_replace_all("_summarised", "")
+  df <- read_tsv(fn, show_col_types = F)
   n_sentences <- nrow(df)
 
   df_unnested <- df %>% 
     mutate(feats_per_token = str_split(feats_per_token, ";")) %>% 
     unnest(feats_per_token) 
   
-  mean_feats_per_token <- mean(df_unnested$feats_per_token)
+  df_spec <- data.frame(dir = dir, 
+                        n_sentences =nrow(df),
+                         sum_tokens = sum(df$n_tokens), 
+                         mean_feats_per_token =  mean(as.numeric(df_unnested$feats_per_token))
+                        )
+  
+  df_all <- df_spec %>% 
+    full_join(df_all, by = join_by(dir, n_sentences, sum_tokens, mean_feats_per_token))
   
 }
 
-mean_feats_per_token_per_lang <-  counts_unnested %>% 
-  full_join(UD_langs, relationship = "many-to-many", by = "dir") %>% 
-  mutate(feats_per_token = as.numeric(feats_per_token)) %>% 
-  group_by(glottocode) %>% 
-    summarise(mean_feats_per_token = mean(feats_per_token))
-
-joined <- counts_df %>% 
-  full_join(UD_langs, relationship = "many-to-many", by = "dir") %>% 
-  group_by(glottocode) %>% 
-  summarise(sum_tokens = sum(n_tokens), .groups = "drop") %>% 
-  left_join(mean_feats_per_token_per_lang, by = join_by(glottocode)) %>% 
+joined <- df_all %>% 
+  left_join(UD_langs, relationship = "many-to-many", by = "dir") %>% 
   left_join(Glottolog, by = "glottocode") %>% 
+  distinct(dir, n_sentences, sum_tokens, mean_feats_per_token, glottocode, Longitude, Latitude) %>% 
   dplyr::mutate(Longitude = if_else(Longitude <= -25, Longitude + 360, Longitude))  #shifting the longlat 
-
-basemap <- SH.misc::basemap_EEZ(, south = "down", colour_border_land = "white", colour_border_eez = "lightgray") 
 
 basemap +
   geom_jitter(data = joined, stat = "identity", 
@@ -53,11 +50,11 @@ basemap +
                  size = sum_tokens,
                  fill = sum_tokens),
              shape = 21, 
-             alpha = 0.8, 
-             stroke = 0.4, 
+             alpha = 0.7, 
+             stroke = 0.5, 
              color = "grey44") +
-  scale_fill_viridis_c(direction = -1) +
-  ggtitle("Tokens per language (across different UD-collections)")
+  scale_fill_viridis_c(direction = -1, end = 0.9) +
+  ggtitle("Number of tokens per UD treebank v2.14")
 
 ggsave("output/plots/map_tokens_per_language.png", height = 5, width = 7)
 
@@ -73,7 +70,7 @@ basemap +
               stroke = 0.4, 
               color = "grey44") +
   scale_fill_viridis_c(direction = -1)  +
-  ggtitle("Mean number of morph feats per token per language (across different UD-collections)")
+  ggtitle("Mean number of morph feats per token per UD treebank v2.14")
 
-ggsave("output/plots/map_mean_feats_per_token_per_language.png", height = 5, width = 7)
+ggsave("output/plots/map_mean_feats_per_token_per_treebank.png", height = 5, width = 7)
 
