39b7c644c9d9021a0ba8683907fd24c10a544afb
HedvigS <hedvig.skirgard@gmail.com>
2024-10-28 18:41:25 +0100

    Update 04_UPOS_feat_usefulness.R

--- code/04_UPOS_feat_usefulness.R
+++ code/04_UPOS_feat_usefulness.R
@@ -1,7 +1,9 @@
 source("01_requirements.R")
 
 Glottolog <- read_tsv("../data/glottolog_language_table_wide_df_3.0.tsv", show_col_types = F) %>% 
-  dplyr::select(glottocode = Glottocode, Longitude, Latitude, Family_ID, Language_level_ID, Name)
+  dplyr::select(glottocode = Glottocode, Longitude, Latitude, Family_ID, Language_level_ID, Name) %>% 
+  dplyr::mutate(Longitude = if_else(Longitude <= -25, Longitude + 360, Longitude))  #shifting the longlat 
+
 
 UD_langs <- read_tsv("../data/UD_languages.tsv", show_col_types = F) %>% 
   dplyr::select(dir,  glottocode)
@@ -14,14 +16,59 @@ stacked$dir <-   stacked$filename%>% basename() %>% str_replace_all(".tsv", "") 
 
 
 summed <- stacked %>% 
-  group_by(filename, upos, feat) %>% 
-  slice_max(prop)
+  filter(feat_value != "unassigned") %>% 
+  dplyr::select(dir, upos, feat, feat_value, n) %>%
+  group_by(dir, upos, feat) %>% 
+  mutate(sum = sum(n)) %>% 
+  ungroup() %>% 
+  mutate(prop = n/sum) %>% 
+  group_by(dir, upos, feat) %>% 
+  slice_max(prop) %>% 
+  left_join(UD_langs, relationship = "many-to-many", by = "dir") %>% 
+  left_join(Glottolog, relationship = "many-to-many", by ="glottocode")
 
-summed$dir <- fct_reorder(summed$dir, summed$prop)
+Adj_polar_df <- summed %>% 
+  filter(upos =="ADJ") %>% 
+  filter(feat =="Polarity") 
 
-summed %>% 
-  filter(upos =="NOUN") %>% 
-  filter(feat =="Definite") %>% 
-  ggplot() +
-  geom_bar(aes(x = dir, y = prop), stat = "identity")
+basemap +
+  geom_jitter(data = Adj_polar_df, stat = "identity", 
+              position = position_jitter(width = 2, height = 2, seed = 198 #jittering the points to prevent overplotting
+              ),
+              aes(x=Longitude, 
+                  y=Latitude,
+                  fill = prop),
+              shape = 21, 
+              alpha = 0.8, 
+              stroke = 0.4, 
+              color = "grey44") +
+  scale_fill_viridis_c(direction = -1, end = 0.9) +
+  theme(legend.position = "bottom") +
+  ggtitle("Usefulness of ADJ polarity marking, UD treebank v2.14")
+
+ggsave("output/plots/map_usefulness_ADJ_polar.png")
+
   
+
+
+Noun_num_df <- summed %>% 
+  filter(upos =="NOUN") %>% 
+  filter(feat =="Number") 
+
+
+basemap +
+  geom_jitter(data = Noun_num_df , stat = "identity", 
+              position = position_jitter(width = 2, height = 2, seed = 198 #jittering the points to prevent overplotting
+              ),
+              aes(x=Longitude, 
+                  y=Latitude,
+                  fill = prop),
+              shape = 21, 
+              alpha = 0.8, 
+              stroke = 0.4, 
+              color = "grey44") +
+  scale_fill_viridis_c(direction = -1) +
+  theme(legend.position = "bottom") +
+  ggtitle("Usefulness of NOUN number marking, UD treebank v2.14")
+
+ggsave("output/plots/map_usefulness_noun_number.png")
