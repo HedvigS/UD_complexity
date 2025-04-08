4229048c52446d5aeaa6b501198f696026bffd69
HedvigS <hedvig.skirgard@gmail.com>
2025-04-08 22:39:56 +0200

    Update 00_run_all.R

--- code/00_run_all.R
+++ code/00_run_all.R
@@ -7,13 +7,12 @@ source("02_google_pop.R")
 source("02_collapse_UD_dirs.R")
 
 source("02_process_data_per_UD_proj.R")
-
 process_data_per_UD_proj(directory = "output", agg_level = "upos", core_features = "core_features_only")
-
-
+process_data_per_UD_proj(directory = "output", agg_level = "lemma", core_features = "core_features_only")
+process_data_per_UD_proj(directory = "output", agg_level = "upos", core_features = "all_features")
+process_data_per_UD_proj(directory = "output", agg_level = "lemma", core_features = "all_features")
 
 source("03_summarise_for_plots.R")
-
 
 source("04_maps_counts.R")
 source("04_map_surprisal.R")
