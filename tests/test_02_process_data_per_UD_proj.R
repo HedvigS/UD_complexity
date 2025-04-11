063f457a92e6abc9aeba696e32819e98e15dc186
Stephen Mann <stephenfmann@gmail.com>
2025-04-11 11:57:47 +0200

    Create test data manually in test script

--- tests/test_02_process_data_per_UD_proj.R
+++ tests/test_02_process_data_per_UD_proj.R
@@ -24,6 +24,34 @@ DIR_S_TOKEN <- "output_test/surprisal_per_token/"
 DIR_S_TOKEN_SUM <- "output_test/surprisal_per_token_sum_sentence/"
 DIR_TTR <- "output_test/TTR/"
 
+# Create test data.
+# First define the filepath for the TSV file that will serve as test data input for the function.
+fpath_out_test <- paste0("output_test/processed_data/", UD_version, "/test_01.tsv")
+
+# Now define a dataframe with headers:
+# "id"	"doc_id"	"paragraph_id"	"sentence_id"	"sentence"	"token_id"	"token"	"lemma"	"upos"	"xpos"	"feats"	"head_token_id"	"dep_rel"	"deps"	"misc"
+test_data <- data.frame(
+  id = c("TEST_01_001", "TEST_01_002", "TEST_01_003", "TEST_01_004", "TEST_02_001", "TEST_02_002", "TEST_02_003", "TEST_02_004", "TEST_03_001", "TEST_03_002", "TEST_03_003"),
+  doc_id = rep("", 11), # leave doc_id column empty for now
+  paragraph_id = rep("000", 11), # don't need a real paragraph ID
+  sentence_id = c(rep("TEST_01",4), rep("TEST_02",4), rep("TEST_03",3)),
+  sentence = c(rep("01 This is a test.",4), rep("02 This is a test.",4), rep("03 This is a test.",3)),
+  token_id = c(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3),
+  token = c("ēkal","x","mār","Aššur-dan","ēkal","Aššur-naṣir-apli","šar","kiššati","marks","mark","marks"),
+  lemma = c("ēkallu","x","māru","Aššur-dan_II","ēkallu","Aššur-naṣir-apli_II","šarru","kiššatu","mark","mark","mark"),
+  upos = c("NOUN","PROPN","NOUN","PROPN","NOUN","PROPN","NOUN","NOUN","VERB","VERB","NOUN"),
+  xpos = c("N","u","N","RN","N","RN","N","N","V","V","N"),
+  feats = c("Gender=Fem|NounBase=Bound|Number=Sing", "Gender=Masc", "Gender=Masc|NounBase=Bound|Number=Sing", "Gender=Masc", "Gender=Fem|NounBase=Bound|Number=Sing", "Gender=Masc", "Gender=Masc|NounBase=Bound|Number=Sing", "Case=Gen|Gender=Fem|NounBase=Free|Number=Sing", "Person=3|Number=Plural", "Person=2", "Number=Plural"),
+  head_token_id = c("0","1","2","3","0","1","2","3","0","0","0"),
+  dep_rel = c("root", "nmod:poss", "appos", "nmod:poss", "root", "nmod:poss", "appos", "nmod:poss", "TEST","TEST", "TEST"),
+  deps = rep("", 11), # leave deps column empty for now
+  misc = rep("test misc string",11), # leave misc column as test string for now
+  stringsAsFactors = FALSE
+)
+
+# Write the test data to a TSV file
+write.table(test_data, fpath_out_test, sep = "\t", row.names = FALSE, quote = FALSE)
+
 # Test case 1: UPOS, core only
 test_that("Test process_data_per_UD_proj: test data, per UPOS, core only",{
   process_data_per_UD_proj(directory = "output_test",agg_level = "upos",core_features = "core_features_only")
