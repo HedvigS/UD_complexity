1e6cb510ba16411dd238d1ef57a76088bee1ac4e
HedvigS <hedvig.skirgard@gmail.com>
2025-04-15 12:28:26 +0200

    comment out things that arent necessary to run now

--- code/00_run_all.R
+++ code/00_run_all.R
@@ -2,7 +2,6 @@
 source("01_requirements.R")
 
 source("02_get_grambank_data.R")
-source("02_google_pop.R")
 
 source("02_collapse_UD_dirs.R")
 
@@ -12,9 +11,11 @@ process_data_per_UD_proj(directory = "output", agg_level = "lemma", core_feature
 process_data_per_UD_proj(directory = "output", agg_level = "upos", core_features = "all_features")
 process_data_per_UD_proj(directory = "output", agg_level = "lemma", core_features = "all_features")
 
-source("03_summarise_for_plots.R")
+#source("03_summarise_for_plots.R")
 
-source("04_maps_counts.R")
-source("04_map_surprisal.R")
-source("04_map_TTR.R")
-source("04_UPOS_feat_usefulness.R")
+#source("04_maps_counts.R")
+#source("04_map_surprisal.R")
+#source("04_map_TTR.R")
+#source("04_UPOS_feat_usefulness.R")
+
+#source("02_google_pop.R")
