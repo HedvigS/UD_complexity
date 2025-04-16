88ca84e4f4a036e8d5e30bd7b7b3ffbd242b88b6
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2025-04-16 10:55:17 +0200

    creat script to download UD dirs

---
+++ code/02_get_UD.R
@@ -0,0 +1,18 @@
+source("01_requirements.R")
+
+if(!file.exists("../data/ud-treebanks-v2.14/UD_Abkhaz-AbNC/ab_abnc-ud-test.txt")){
+
+options(timeout = 300) 
+  
+SH.misc::get_zenodo_dir(url = "https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-5502/allzip", 
+                        exdir= "../data/UD_zip_files/",drop_dir_level = F)
+
+utils::untar(tarfile =  "../data/UD_zip_files/ud-treebanks-v2.14.tgz", exdir = "../data/")
+
+}
+
+if(283 == list.files("../data/ud-treebanks-v2.14/") %>% length()){
+  cat("../data/ud-treebanks-v2.14 contains 283 files, looks good!")
+}else{
+  warning("../data/ud-treebanks-v2.14 does not contain 283 files, something is wrong!")
+  }
