5f64f30939606ff8e4037ce0b9fe83d753ec35f5
HedvigS <hedvig.skirgard@gmail.com>
2025-04-19 15:51:57 +0200

    Update 00_run_all.R

--- code/00_run_all.R
+++ code/00_run_all.R
@@ -1,10 +1,12 @@
 
 source("01_requirements.R")
 
-source("02_get_UD.R")
-source("02_collapse_UD_dirs.R")
 source("02_get_grambank_data.R")
 source("02_google_pop.R")
+source("02_get_glottolog_language_table.R")
+
+source("02_get_UD.R")
+source("02_collapse_UD_dirs.R")
 
 source("03_process_data_per_UD_proj.R")
 process_data_per_UD_proj(directory = "output", agg_level = "upos", core_features = "core_features_only")
