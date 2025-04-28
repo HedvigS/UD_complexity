e357bb1c0171e024d2401e923fac12152781cd72
HedvigS <hedvig.skirgard@gmail.com>
2025-04-29 00:38:24 +0200

    Update 04_plot_SPLOM.R

--- code/04_plot_SPLOM.R
+++ code/04_plot_SPLOM.R
@@ -1,41 +1,6 @@
 source("01_requirements.R")
 source("fun_def_SPLOM_fun.R")
 
-# Create colour-vector function
-create_colour_vector <- function(base_colours, n_variables) {
-  # The number of squares is the Nth triangular number
-  # The formula for the Nth triangular number is N*(N+1)/2
-  n_squares <- n_variables * (n_variables + 1) / 2
-  
-  # Initialise the colours vector
-  colours <- c()
-  
-  # Initialise the number of squares per row
-  n_squares_per_row <- n_variables - 1
-
-  # Set the start index of the colours vector
-  i_colour_start <- 1
-
-  # The number of rows is N-1
-  for (i in 1:(n_variables - 1)) {
-    
-    # Put n_squares_per_row squares in the row by repeating the base colours
-    colours <- c(colours, rep(base_colours, length.out = n_squares_per_row))
-
-    # Increment the start index of the colours vector
-    i_colour_start <- i_colour_start + n_squares_per_row
-
-    # Decrease the number of squares per row by 1
-    n_squares_per_row <- n_squares_per_row - 1
-
-    # Permute the base colours by 2
-    base_colours <- base_colours[c(3, 4, 5, 1, 2)]
-
-  }
-  
-  return(colours)
-}
-
 ##################################
 
 UD_langs <- read_tsv("../data/UD_languages.tsv", show_col_types = F) %>% 
@@ -51,7 +16,7 @@ df <- read_tsv(file = "output/all_summaries_stacked.tsv", show_col_types = F) %>
   filter(n_feat_cats_all_features != 0) %>% 
   filter(n_feat_cats_core_features_only != 0) %>% 
   distinct()
-
+ 
 df_for_plot <- df %>% 
   dplyr::select("Surprisal feat\nagg_level = lemma\nall features" = "sum_surprisal_morph_split_mean_lemma_all_features"  ,         
                 "Surprisal feat\nagg_level = lemma\ncore features only"  = "sum_surprisal_morph_split_mean_lemma_core_features_only"    ,  
@@ -86,38 +51,41 @@ ggsave("output/plots/SPLOM_custom_metrics.png", height = 30, width = 30, units =
 df_for_plot <- df %>% 
   dplyr::select("Surprisal feat\nagg_level = UPOS\nall features" = "sum_surprisal_morph_split_mean_upos_all_features" , 
                 "Surprisal featstring\nagg_level = lemma\ncore features only" = "surprisal_per_morph_featstring_mean_lemma_core_features_only",
+                "TTR",
+                "LTR"   ,
+                "Feats per token\n(mean)\nall features" = "n_feats_per_token_mean_all_features",                         
+                "Feats per token\n(mean)\ncore features only" ="n_feats_per_token_mean_core_features_only",
+                "Suprisal of token\nmean" = suprisal_token_mean, 
                 "Types (n)" = "n_types", 
                 "Tokens (n) " = "n_tokens",
                 "Sentences (n)" = "n_sentences" ,
-                "TTR",
-                "LTR"   ,
                 "Feat cat (n)\nall features" = "n_feat_cats_all_features",                                    
-                "Feat cat (n)\ncore features only" = "n_feat_cats_core_features_only",                               
-                "Feats per token(n)(mean)\nall features" = "n_feats_per_token_mean_all_features",                         
-                "Feats per token(n)(mean)\ncore features only" ="n_feats_per_token_mean_core_features_only"
+                "Feat cat (n)\ncore features only" = "n_feat_cats_core_features_only"
+                
             ) 
 
 pal <- c(sum_surprisal_morph_split_mean_upos_all_features_col, #1
          surprisal_per_morph_featstring_mean_lemma_core_features_only_col,   #2
-         "#E278B1", #3
+         TTR_col, #3
          "#BA4FE1", #4
          "#cf2d27" , #5
-         TTR_col, #6
+         "#E278B1", #6
          "#ed268d", #7
          "#c735e8", #8
          "#f723bb", #9
          "#cf2757", #10
-         "grey40" #11
+         "#c387f5", #11
+         "grey40" #12
 )
 
 
 p <-  df_for_plot %>% 
-  coloured_SPLOM(hist_label_size = 2.5, text_cor_size = 5, text_strip_size = 6, pair_colors = pal, herringbone = T
+  coloured_SPLOM(hist_label_size = 2.3, text_cor_size = 5, text_strip_size = 6, pair_colors = pal, herringbone = T
                  )
 
 
 df_for_plot %>% 
-  coloured_SPLOM(hist_label_size = 2.5, text_cor_size = 5, text_strip_size = 6, herringbone = T
+  coloured_SPLOM(hist_label_size = 2.5, text_cor_size = 5, text_strip_size = 7, herringbone = T
   )
 
 
@@ -174,16 +142,19 @@ df_for_plot <- df %>%
                 "Surprisal featstring\nagg_level = UPOS\nall features" = "surprisal_per_morph_featstring_mean_upos_all_features",       
                 "Surprisal featstring\nagg_level = UPOS\ncore features only" = "surprisal_per_morph_featstring_mean_upos_core_features_only" ) 
 
-# Set the number of variables
-n_variables <- 8
 
-# Create the colour vector
-colors <- create_colour_vector(base_colours, n_variables)
-
-# colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7", "#D6AF87", "#D03AE7", "#BABFE5", "#D567DD", "#C0E7B5", "#E7C3D1", "#D9D377")
+pal <- c("#A7E1A1" , #1
+         "#5bafe3",   #2
+         sum_surprisal_morph_split_mean_upos_all_features_col, #3
+         "#90D8D7", #4
+         "#86e397" , #5
+         surprisal_per_morph_featstring_mean_lemma_core_features_only_col, #6
+         "#35c43f", #7
+         "grey40" #8
+)
 
 p <- df_for_plot %>% 
-  coloured_SPLOM(pair_colors = colors, text_cor_size = 5, text_strip_size = 10, hist_label_size = 2.5)
+  coloured_SPLOM(pair_colors = pal, text_cor_size = 5, text_strip_size = 10, hist_label_size = 2.5, herringbone = T)
 
 ggsave("output/plots/SPLOM_custom_metrics_PUD.png", height = 30, width = 30, units = "cm", plot = p)
 
@@ -191,27 +162,35 @@ df_for_plot <- df %>%
   filter(str_detect(dir, pattern = "PUD")) %>% 
   dplyr::select("Surprisal feat\nagg_level = UPOS\nall features" = "sum_surprisal_morph_split_mean_upos_all_features" , 
                 "Surprisal featstring\nagg_level = lemma\ncore features only" = "surprisal_per_morph_featstring_mean_lemma_core_features_only",
-                "Types (n)" = "n_types", 
-                "Tokens (n) " = "n_tokens",
-                #"Sentences (n)" = "n_sentences" ,
                 "TTR",
                 "LTR"   ,
+                "Feats per token\n(mean)\nall features" = "n_feats_per_token_mean_all_features",                         
+                "Feats per token\n(mean)\ncore features only" ="n_feats_per_token_mean_core_features_only",
+                "Suprisal of token\nmean" = suprisal_token_mean, 
+                "Types (n)" = "n_types", 
+                "Tokens (n) " = "n_tokens",
+              #  "Sentences (n)" = "n_sentences" ,
                 "Feat cat (n)\nall features" = "n_feat_cats_all_features",                                    
-                "Feat cat (n)\ncore features only" = "n_feat_cats_core_features_only",                               
-                "Feats per token(n)(mean)\nall features" = "n_feats_per_token_mean_all_features",                         
-                "Feats per token(n)(mean)\ncore features only" ="n_feats_per_token_mean_core_features_only"
+                "Feat cat (n)\ncore features only" = "n_feat_cats_core_features_only"
+                
   ) 
 
-# Set the number of variables
-n_variables <- 10
-
-# Create the colour vector
-colors <- create_colour_vector(base_colours, n_variables)
-
-# colors <-  c("#CBF1B5", "#E6ADC8", "#8A41A4", "#5C65DF", "#E276D7", "#69B9DE", "#6271B8", "#559EE8", "#A8BFEF", "#61E66C", "#5CE896", "#EDE9A9", "#E581B4", "#5BE939", "#E9B2A8", "#7E34E2", "#D2F62E", "#9BCF89", "#B08CE6", "#B4ECDF", "#B1AE8A", "#E0EC7F", "#A39C9D", "#94EDC0", "#E0EDCE", "#C7AFE5", "#44A96B", "#93605C", "#53725B", "#BCB5CA", "#7BB79F", "#959747", "#E547DA", "#ECD5EC", "#B0EA56", "#50748D", "#59DDDA", "#E68992", "#A177AA", "#E3C763", "#4FEAC5", "#E0A967", "#CE4086", "#7CB633", "#E2CAA0")
+pal <- c(sum_surprisal_morph_split_mean_upos_all_features_col, #1
+         surprisal_per_morph_featstring_mean_lemma_core_features_only_col,   #2
+         TTR_col, #3
+         "#BA4FE1", #4
+         "#cf2d27" , #5
+         "#E278B1", #6
+         "#ed268d", #7
+         "#c735e8", #8
+         "#f723bb", #9
+         "#cf2757", #10
+         "#c387f5", #11
+         "grey40" #12
+)
 
 p <-  df_for_plot %>% 
-  coloured_SPLOM(hist_label_size = 2.5, text_cor_size = 5, text_strip_size = 6, pair_colors = colors)
+  coloured_SPLOM(hist_label_size = 2.3, text_cor_size = 5, text_strip_size = 7, pair_colors = pal, herringbone = T)
 
 ggsave("output/plots/SPLOM_other_metrics_PUD.png", height = 30, width = 30, units = "cm", plot = p)
 
@@ -228,21 +207,25 @@ df_for_plot <- df %>%
   mutate("Pop\n(Google)\nlog10" = log10(`Pop\n (Google)` +1)) %>% 
   distinct()
 
-# Set the number of variables
-n_variables <- 7
-
-# Create the colour vector
-colors <- create_colour_vector(base_colours, n_variables)
-
-# colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7")
+pal <- c(sum_surprisal_morph_split_mean_upos_all_features_col, #1
+         surprisal_per_morph_featstring_mean_lemma_core_features_only_col,   #2
+         TTR_col, #3
+         "#DBAC5E", #4
+         "#f5dd02", #5
+         "#f57f31", #6
+         "grey40"
+)
 
 p <-df_for_plot %>% 
   coloured_SPLOM(hist_label_size = 3,
-                 pair_colors = colors, 
+                 pair_colors = pal, 
                  text_cor_size = 5, 
                  text_strip_size = 10,
-                 col_pairs_to_constraint = c("Fusion", "Informativity", "Pop\n (Google)", "Pop\n(Google)\nlog10"), 
-                 col_pairs_constraint = "glottocode")
+                 col_pairs_to_constraint = c("Fusion\n(Grambank v1.0)", 
+                                             "Informativity\n(Grambank v1.0)", 
+                                             "Pop\n (Google)", 
+                                             "Pop\n(Google)\nlog10"), 
+                 col_pairs_constraint = "glottocode", herringbone = T)
 
 ggsave("output/plots/SPLOM_metrics_external_PUD.png", height = 30, width = 30, units = "cm", plot = p)
 
