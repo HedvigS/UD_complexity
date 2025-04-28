353218aa9e36023884e1b34606a8c053e4c29cad
Stephen Mann <stephenfmann@gmail.com>
2025-04-28 15:22:21 +0200

    SPLOM colours diagonalised

--- code/04_plot_SPLOM.R
+++ code/04_plot_SPLOM.R
@@ -1,6 +1,43 @@
 source("01_requirements.R")
 source("fun_def_SPLOM_fun.R")
 
+# Create colour-vector function
+create_colour_vector <- function(base_colours, n_variables) {
+  # The number of squares is the Nth triangular number
+  # The formula for the Nth triangular number is N*(N+1)/2
+  n_squares <- n_variables * (n_variables + 1) / 2
+  
+  # Initialise the colours vector
+  colours <- c()
+  
+  # Initialise the number of squares per row
+  n_squares_per_row <- n_variables - 1
+
+  # Set the start index of the colours vector
+  i_colour_start <- 1
+
+  # The number of rows is N-1
+  for (i in 1:(n_variables - 1)) {
+    
+    # Put n_squares_per_row squares in the row by repeating the base colours
+    colours <- c(colours, rep(base_colours, length.out = n_squares_per_row))
+
+    # Increment the start index of the colours vector
+    i_colour_start <- i_colour_start + n_squares_per_row
+
+    # Decrease the number of squares per row by 1
+    n_squares_per_row <- n_squares_per_row - 1
+
+    # Permute the base colours by 2
+    base_colours <- base_colours[c(3, 4, 5, 1, 2)]
+
+  }
+  
+  return(colours)
+}
+
+##################################
+
 UD_langs <- read_tsv("../data/UD_languages.tsv", show_col_types = F) %>% 
   dplyr::select(dir, glottocode)
 
@@ -25,7 +62,34 @@ df_for_plot <- df %>%
                 "Surprisal featstring\nagg_level = UPOS\nall features" = "surprisal_per_morph_featstring_mean_upos_all_features",       
                 "Surprisal featstring\nagg_level = UPOS\ncore features only" = "surprisal_per_morph_featstring_mean_upos_core_features_only" ) 
 
-colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7", "#D6AF87", "#D03AE7", "#BABFE5", "#D567DD", "#C0E7B5", "#E7C3D1", "#D9D377")
+
+# colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580",
+#              "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF",
+#              "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0",
+#              "#7045D9", "#9A6E7E", "#E2E0C7", "#D6AF87",
+#              "#D03AE7", "#BABFE5", "#D567DD",
+#              "#C0E7B5", "#E7C3D1",
+#              "#D9D377")
+
+
+
+# Make the triangle from the base colours.
+base_colours <- c("#DC8F3D", "#5FE3B6", "#E55679", "#64BFDB", "#7045D9")
+
+# Set the number of variables
+n_variables <- 8
+
+# Create the colour vector
+colors <- create_colour_vector(base_colours, n_variables)
+
+# Should be:
+# colors <-  c("#DC8F3D", "#5FE3B6", "#E55679", "#64BFDB", "#7045D9", "#DC8F3D", "#5FE3B6", 
+#                         "#E55679", "#64BFDB", "#7045D9", "#DC8F3D", "#5FE3B6", "#E55679", 
+#                                    "#7045D9", "#DC8F3D", "#5FE3B6", "#E55679", "#64BFDB", 
+#                                               "#5FE3B6", "#E55679", "#64BFDB", "#7045D9", 
+#                                                          "#64BFDB", "#7045D9", "#DC8F3D", 
+#                                                                     "#DC8F3D", "#5FE3B6", 
+#                                                                                "#E55679")
 
 p <- df_for_plot %>% 
     coloured_SPLOM(pair_colors = colors, text_cor_size = 5, text_strip_size = 10, hist_label_size = 2.5)
@@ -46,8 +110,13 @@ df_for_plot <- df %>%
                 "Feats per token(n)(mean)\ncore features only" ="n_feats_per_token_mean_core_features_only"
             ) 
 
+# Set the number of variables
+n_variables <- 11
 
-colors <-  c("#CBF1B5", "#E6ADC8", "#8A41A4", "#5C65DF", "#E276D7", "#69B9DE", "#6271B8", "#559EE8", "#A8BFEF", "#61E66C", "#5CE896", "#EDE9A9", "#E581B4", "#5BE939", "#E9B2A8", "#7E34E2", "#D2F62E", "#9BCF89", "#B08CE6", "#B4ECDF", "#B1AE8A", "#E0EC7F", "#A39C9D", "#94EDC0", "#E0EDCE", "#C7AFE5", "#44A96B", "#93605C", "#53725B", "#BCB5CA", "#7BB79F", "#959747", "#E547DA", "#ECD5EC", "#B0EA56", "#50748D", "#59DDDA", "#E68992", "#A177AA", "#E3C763", "#4FEAC5", "#E0A967", "#CE4086", "#7CB633", "#E2CAA0", "#E85158", "#E0EEF1", "#E48449", "#B55DE3", "#89DCEA", "#E7A1E5", "#EDDED2", "#B6D3E2", "#A0E981", "#E6DF41")
+# Create the colour vector
+colors <- create_colour_vector(base_colours, n_variables)
+
+# colors <-  c("#CBF1B5", "#E6ADC8", "#8A41A4", "#5C65DF", "#E276D7", "#69B9DE", "#6271B8", "#559EE8", "#A8BFEF", "#61E66C", "#5CE896", "#EDE9A9", "#E581B4", "#5BE939", "#E9B2A8", "#7E34E2", "#D2F62E", "#9BCF89", "#B08CE6", "#B4ECDF", "#B1AE8A", "#E0EC7F", "#A39C9D", "#94EDC0", "#E0EDCE", "#C7AFE5", "#44A96B", "#93605C", "#53725B", "#BCB5CA", "#7BB79F", "#959747", "#E547DA", "#ECD5EC", "#B0EA56", "#50748D", "#59DDDA", "#E68992", "#A177AA", "#E3C763", "#4FEAC5", "#E0A967", "#CE4086", "#7CB633", "#E2CAA0", "#E85158", "#E0EEF1", "#E48449", "#B55DE3", "#89DCEA", "#E7A1E5", "#EDDED2", "#B6D3E2", "#A0E981", "#E6DF41")
 
 p <-  df_for_plot %>% 
   coloured_SPLOM(hist_label_size = 2.5, text_cor_size = 5, text_strip_size = 6, pair_colors = colors)
@@ -66,7 +135,13 @@ df_for_plot <- df %>%
   mutate("Pop\n(Google)\nlog10" = log10(`Pop\n (Google)` +1)) %>% 
   distinct()
 
-colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7")
+# Set the number of variables
+n_variables <- 7
+
+# Create the colour vector
+colors <- create_colour_vector(base_colours, n_variables)
+
+# colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7")
 
 p <-df_for_plot %>% 
   coloured_SPLOM(hist_label_size = 3,
@@ -92,7 +167,13 @@ df_for_plot <- df %>%
                 "Surprisal featstring\nagg_level = UPOS\nall features" = "surprisal_per_morph_featstring_mean_upos_all_features",       
                 "Surprisal featstring\nagg_level = UPOS\ncore features only" = "surprisal_per_morph_featstring_mean_upos_core_features_only" ) 
 
-colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7", "#D6AF87", "#D03AE7", "#BABFE5", "#D567DD", "#C0E7B5", "#E7C3D1", "#D9D377")
+# Set the number of variables
+n_variables <- 8
+
+# Create the colour vector
+colors <- create_colour_vector(base_colours, n_variables)
+
+# colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7", "#D6AF87", "#D03AE7", "#BABFE5", "#D567DD", "#C0E7B5", "#E7C3D1", "#D9D377")
 
 p <- df_for_plot %>% 
   coloured_SPLOM(pair_colors = colors, text_cor_size = 5, text_strip_size = 10, hist_label_size = 2.5)
@@ -114,8 +195,13 @@ df_for_plot <- df %>%
                 "Feats per token(n)(mean)\ncore features only" ="n_feats_per_token_mean_core_features_only"
   ) 
 
+# Set the number of variables
+n_variables <- 10
 
-colors <-  c("#CBF1B5", "#E6ADC8", "#8A41A4", "#5C65DF", "#E276D7", "#69B9DE", "#6271B8", "#559EE8", "#A8BFEF", "#61E66C", "#5CE896", "#EDE9A9", "#E581B4", "#5BE939", "#E9B2A8", "#7E34E2", "#D2F62E", "#9BCF89", "#B08CE6", "#B4ECDF", "#B1AE8A", "#E0EC7F", "#A39C9D", "#94EDC0", "#E0EDCE", "#C7AFE5", "#44A96B", "#93605C", "#53725B", "#BCB5CA", "#7BB79F", "#959747", "#E547DA", "#ECD5EC", "#B0EA56", "#50748D", "#59DDDA", "#E68992", "#A177AA", "#E3C763", "#4FEAC5", "#E0A967", "#CE4086", "#7CB633", "#E2CAA0")
+# Create the colour vector
+colors <- create_colour_vector(base_colours, n_variables)
+
+# colors <-  c("#CBF1B5", "#E6ADC8", "#8A41A4", "#5C65DF", "#E276D7", "#69B9DE", "#6271B8", "#559EE8", "#A8BFEF", "#61E66C", "#5CE896", "#EDE9A9", "#E581B4", "#5BE939", "#E9B2A8", "#7E34E2", "#D2F62E", "#9BCF89", "#B08CE6", "#B4ECDF", "#B1AE8A", "#E0EC7F", "#A39C9D", "#94EDC0", "#E0EDCE", "#C7AFE5", "#44A96B", "#93605C", "#53725B", "#BCB5CA", "#7BB79F", "#959747", "#E547DA", "#ECD5EC", "#B0EA56", "#50748D", "#59DDDA", "#E68992", "#A177AA", "#E3C763", "#4FEAC5", "#E0A967", "#CE4086", "#7CB633", "#E2CAA0")
 
 p <-  df_for_plot %>% 
   coloured_SPLOM(hist_label_size = 2.5, text_cor_size = 5, text_strip_size = 6, pair_colors = colors)
@@ -135,7 +221,13 @@ df_for_plot <- df %>%
   mutate("Pop\n(Google)\nlog10" = log10(`Pop\n (Google)` +1)) %>% 
   distinct()
 
-colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7")
+# Set the number of variables
+n_variables <- 7
+
+# Create the colour vector
+colors <- create_colour_vector(base_colours, n_variables)
+
+# colors <-  c("#E55679", "#5FE3B6", "#D447A0", "#B1E586", "#D8A8DE", "#E5E145", "#63E580", "#74E1DA", "#889C8B", "#94E949", "#E58F8F", "#DC8F3D", "#7295CF", "#C6E5E9", "#699D55", "#64BFDB", "#E489D4", "#8E7CE0", "#7045D9", "#9A6E7E", "#E2E0C7")
 
 p <-df_for_plot %>% 
   coloured_SPLOM(hist_label_size = 3,
