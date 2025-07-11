f4ec57af20296fd3b4e40d5caf12e70713a31938
HedvigS <hedvig.skirgard@gmail.com>
2025-07-11 11:51:40 +0200

    move check_functions to sandbox

--- code/01_check_used_functions.R
+++
@@ -1,5 +0,0 @@
-source("01_requirements.R")
-
-fns <- list.files(path = ".", all.files = T, full.names = T, recursive = F, pattern = ".R$")
-
-SH.misc::credit_packages(fns = fns, output_dir = "../latex/")
