5fda445f35b79e2a0fd31dcd6ed30db3bc87fed3
Stephen Mann <stephenfmann@gmail.com>
2025-07-13 19:21:03 +0200

    Requirements for tests installed globally

--- tests/requirements_tests.R
+++ tests/requirements_tests.R
@@ -1,10 +1,21 @@
 UD_version <- "ud-treebanks-v2.14"
 
+# Set CRAN mirror
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
+
+# Install the required packages if they are not already installed.
+required_packages <- c("testthat", "dplyr", "magrittr", "tidyr", "readr", "stringr", "reshape2")
+for (pkg in required_packages) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+}
+
 # # Load required packages
-library(dplyr, lib.loc = "../utility/packages/")
-library(magrittr, lib.loc = "../utility/packages/")
-library(tidyr, lib.loc = "../utility/packages/")
-library(readr, lib.loc = "../utility/packages/")
-library(stringr, lib.loc = "../utility/packages/")
-library(reshape2, lib.loc = "../utility/packages/")
-library(testthat, lib.loc = "../utility/packages/")
+library(testthat)
+library(dplyr)
+library(magrittr)
+library(tidyr)
+library(readr)
+library(stringr)
+library(reshape2)
