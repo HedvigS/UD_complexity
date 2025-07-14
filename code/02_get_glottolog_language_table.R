5266fada7fa81057ee181f781c26af93831e4c20
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2025-07-14 19:17:35 +0200

    udpate 02_get glottolog to make easier for manual fetching

--- code/02_get_glottolog_language_table.R
+++ code/02_get_glottolog_language_table.R
@@ -7,16 +7,24 @@ library(magrittr, lib.loc = "../utility/packages/")
 
 if(!file.exists("output/processed_data/glottolog_5.0_languages.tsv")){
 
-  dir <- paste0("../data/glottolog")
-  if(!dir.exists(dir)){
-    dir.create(dir)}
-    
 # fetching Glottolog v5.0 LanguageTable and ValueTable from GitHub, specifically the branch tied to the release of 5.0
-
-ValueTable_wide <- readr::read_csv("https://github.com/glottolog/glottolog-cldf/raw/refs/tags/v5.0/cldf/values.csv", show_col_types = F) %>% 
+  if(!file.exists("../data/glottolog/values.csv")){
+    readr::read_csv("https://github.com/glottolog/glottolog-cldf/raw/refs/tags/v5.0/cldf/values.csv", 
+                    show_col_types = F) %>% 
+      readr::write_csv("../data/glottolog/values.csv",quote = "all", na = "")
+  }
+  
+  if(!file.exists("../data/glottolog/languages.csv")){
+  
+    readr::read_csv("https://github.com/glottolog/glottolog-cldf/raw/refs/tags/v5.0/cldf/languages.csv",
+                    show_col_types = F) %>% 
+      readr::write_csv("../data/glottolog/languages.csv",quote = "all", na = "")
+    }
+  
+ValueTable_wide <- readr::read_csv("../data/glottolog/values.csv", show_col_types = F) %>% 
   reshape2::dcast(Language_ID ~ Parameter_ID, value.var = "Value")
   
-readr::read_csv("https://github.com/glottolog/glottolog-cldf/raw/refs/tags/v5.0/cldf/languages.csv", show_col_types = F) %>% 
+readr::read_csv("../data/glottolog/languages.csv", show_col_types = F) %>% 
   dplyr::rename(Language_level_ID = Language_ID, Language_ID = ID) %>% 
   dplyr::full_join(ValueTable_wide, by = "Language_ID") %>% 
   readr::write_tsv("output/processed_data/glottolog_5.0_languages.tsv")
