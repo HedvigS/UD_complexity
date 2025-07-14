69943e04809c14e354d252b8ad87bdfa2c0139b7
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2025-07-14 19:17:18 +0200

    splitting out making dirs from 01_requirements.R

---
+++ code/01_requirements_dirs.R
@@ -0,0 +1,18 @@
+
+#setting up folders
+if(!dir.exists("output")){
+  dir.create("output")
+}
+
+dir <- paste0("output/processed_data/")
+if(!dir.exists(dir)){
+  dir.create(dir)
+}
+dir <- paste0("output/plots/")
+if(!dir.exists(dir)){
+  dir.create(dir)
+}
+
+dir <- paste0("../data/glottolog")
+if(!dir.exists(dir)){
+  dir.create(dir)}
