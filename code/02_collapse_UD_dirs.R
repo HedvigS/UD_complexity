4e7659dcad525337f16a939982597736ed6cd603
HedvigS <hedvig.skirgard@gmail.com>
2025-12-11 22:15:25 +0100

    Update 02_collapse_UD_dirs.R

--- code/02_collapse_UD_dirs.R
+++ code/02_collapse_UD_dirs.R
@@ -6,7 +6,7 @@ library(magrittr, lib.loc = "../utility/packages/")
 library(stringr, lib.loc = "../utility/packages/")
 
 #set cut-off for inclusion. number of tokens minially
-minimum_tokens = 40000 # minimum used by Çöltekin & Rama (2023)
+minimum_tokens = 13000 # minimum used by Çöltekin & Rama (2023)
 
 dir <- paste0("output/processed_data/ud-treebanks-v2.14")
 if(!dir.exists(dir)){
@@ -43,9 +43,9 @@ UD_dirs <- UD_langs %>%
 
 for(i in 1:nrow(UD_dirs)){
   
-  #i <- 132
+  #i <- 67
   
-  cat(paste0("I'm on ", UD_dirs[i,1], ". That's ", i, " of ", nrow(UD_dirs), ".\n"))
+  cat(paste0("I'm on ", UD_dirs[i,1], ". That's ", i, " of ", nrow(UD_dirs)))
   
   UD_dir_spec <- UD_dirs[i,] %>% 
     dplyr::mutate(conllus = str_split(conllus, ";")) %>% 
@@ -67,7 +67,8 @@ for(i in 1:nrow(UD_dirs)){
     "head_token_id"  = as.character(),
     "dep_rel"       = as.character(), 
     "deps"       = as.character(),    
-    "misc"    = as.character()
+    "misc"    = as.character(), 
+    "filename"= as.character()
     )
   
 
@@ -77,11 +78,11 @@ for(i in 1:nrow(UD_dirs)){
 
     conllu <- udpipe::udpipe_read_conllu(fn) %>%   
       dplyr::filter(!is.na(token)) %>% 
-#      dplyr::mutate(lemma = ifelse(is.na(lemma), token, lemma)) %>% #if there isn't a lemma assigned, assume that the unique token is the lemma
+      dplyr::mutate(upos = ifelse(str_detect(token_id, "-") & is.na(lemma), "MULTIWORD", upos)) %>% #making missing value upos for multiword token not missing so that it survives the negative filters below
       dplyr::filter(upos != "PUNCT")  %>% #remove tokens that are tagged with the part-of-speech tag punct for punctuation
-      dplyr::filter(upos != "X")  #%>%
-#      dplyr::filter(upos != "SYM") # %>%
-#      dplyr::filter(token != "%") 
+      dplyr::filter(upos != "X")  %>%
+      dplyr::mutate(filename = fn) %>% 
+      dplyr::mutate(upos = ifelse(upos == "MULTIWORD", NA, upos)) #turning missing upos for multiword items back into NA so that the rest of the data-flow works as expected.
 
     if(all(!is.na(conllu$doc_id))){
       suppressWarnings( conllu$doc_id <- stringr::str_pad(as.numeric(conllu$doc_id), width = 3, pad = "0", side = "left") )
@@ -96,17 +97,18 @@ for(i in 1:nrow(UD_dirs)){
   
     df <- df %>% 
       dplyr::full_join(conllu, by = dplyr::join_by(id, doc_id, paragraph_id, sentence_id, sentence, token_id, token, lemma, upos, xpos, feats, head_token_id,
-                                     dep_rel, deps, misc))
+                                     dep_rel, deps, misc, filename))
     
   
   }
 
 if(nrow(df) >= minimum_tokens){  
   
+  cat("\n It is written to file. There were ", nrow(df), " tokens.\n")
   df %>% 
     readr::write_tsv(file = paste0("output/processed_data/ud-treebanks-v2.14", "/", 
                             UD_dir_spec[1,1], ".tsv"), quote = "all", na = "")}else{
-                              warning(paste0( UD_dir_spec[1,1], " has less than ", minimum_tokens, " tokens and is therefore exlcuded."))
-                                                        }
+                              cat("\n Too few tokens, not written to file. There were ", nrow(df), " tokens.\n")
+                            }
   
 }
