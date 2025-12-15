d642399b0ac0db295d5858ca4f51b14cdd20303d
HedvigS <hedvig.skirgard@gmail.com>
2025-12-15 23:29:33 +0100

    Update 04_stack_summaries.R

--- code/04_stack_summaries.R
+++ code/04_stack_summaries.R
@@ -6,25 +6,33 @@ library(magrittr, lib.loc = "../utility/packages/")
 
 source("../utility/SH_misc/stack_tsvs.R")
 
-fns <- list.files(path = "output/summarised/", pattern = "agg_level.*.tsv", full.names = T)
+fns1 <- list.files(path = "output/results/ud-treebanks-v2.14_results/agg_level_lemma_all_features/summarised/", 
+                  pattern = ".tsv", full.names = T)
+fns2 <- list.files(path = "output/results/ud-treebanks-v2.14_results/agg_level_lemma_core_features_only/summarised/", 
+                  pattern = ".tsv", full.names = T)
+fns3 <- list.files(path = "output/results/ud-treebanks-v2.14_results/agg_level_upos_all_features/summarised/", 
+                  pattern = ".tsv", full.names = T)
+fns4 <- list.files(path = "output/results/ud-treebanks-v2.14_results/agg_level_upos_core_features_only/summarised/", 
+                  pattern = ".tsv", full.names = T)
 
-all <- stack_tsvs(fns = fns) 
+fns <- c(fns1, fns2, fns3, fns4)
+
+all <- stack_tsvs(fns = fns, verbose = F) 
 
 custom_metrics_df_4_ways <- all %>% 
   dplyr::select(dir, agg_level, core_features,
     "sum_surprisal_morph_split_mean"     ,
  "surprisal_per_morph_featstring_mean" ) %>% 
   reshape2::melt(id.vars = c("dir", "agg_level", "core_features")) %>%
-  tidyr::unite(variable, agg_level, core_features, col = "variable") %>% 
-  reshape2::dcast(dir ~ variable, value.var = "value")
+  tidyr::unite(variable, agg_level, core_features, col = "cast_variable", sep = "_", remove = TRUE) %>%
+  reshape2::dcast(dir ~ cast_variable, value.var = "value")
 
 metrics_df_2_ways <- all %>% 
-  dplyr::select(dir, core_features,
+  dplyr::select(dir, agg_level, core_features,
 "n_feats_per_token_mean", "n_feat_cats" ) %>% 
-  reshape2::melt(id.vars = c("dir", "core_features")) %>%
-  tidyr::unite(variable, core_features, col = "variable", remove = T) %>% 
-  dplyr::distinct() %>% 
-  reshape2::dcast(dir ~ variable, value.var = "value")
+  reshape2::melt(id.vars = c("dir", "agg_level", "core_features")) %>%
+  tidyr::unite(variable, agg_level, core_features, col = "cast_variable", remove = TRUE) %>% 
+  reshape2::dcast(dir ~ cast_variable, value.var = "value")
 
 df <- all %>% 
   dplyr::select(dir, "n_types", "n_tokens" , "n_sentences" ,"TTR" ,    "LTR"   , "suprisal_token_mean" ) %>% 
@@ -34,4 +42,3 @@ df <- all %>%
   
 df %>% 
   readr::write_tsv("output/all_summaries_stacked.tsv", na = "")
-
