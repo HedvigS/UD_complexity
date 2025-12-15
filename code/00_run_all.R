c7dbd1cbe92d6adf6dbdedf1d70e4e97af7f43ab
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2025-12-15 16:06:29 +0100

    Update 00_run_all.R

--- code/00_run_all.R
+++ code/00_run_all.R
@@ -11,6 +11,17 @@ source("02_get_UD.R")
 source("02_collapse_UD_dirs.R")
 
 source("03_process_data_per_UD_proj.R")
+process_UD_data(input_dir = "output/processed_data/ud-treebanks-v2.14_collapsed/", 
+                output_dir = "output/processed_data/ud-treebanks-v2.14_processed/", 
+                resolve_multiwords_to = "super-word", 
+                remove_empty_nodes = TRUE, 
+                agg_level = "upos", #lemma token,
+                bad_UD_morph_feat_cats =  c("Abbr", "Typo", "Foreign"),
+                core_features = "core_features_only",
+                fill_empty_lemmas_with_tokens = TRUE,
+                make_all_tokens_of_same_agg_level_have_same_feat_cat =  TRUE
+)
+
 process_data_per_UD_proj(directory = "output", agg_level = "upos", core_features = "core_features_only")
 process_data_per_UD_proj(directory = "output", agg_level = "lemma", core_features = "core_features_only")
 process_data_per_UD_proj(directory = "output", agg_level = "upos", core_features = "all_features")
