c57787c30df5e987b467d4ad15f1871332118723
HedvigS <hedvig.skirgard@gmail.com>
2025-12-20 23:11:25 +0100

    Update 02_get_grambank_data.R

--- code/02_get_grambank_data.R
+++ code/02_get_grambank_data.R
@@ -16,24 +16,19 @@ ValueTable <- readr::read_csv("../data/grambank/cldf/values.csv", show_col_types
 LanguageTable <- readr::read_csv("../data/grambank/cldf/languages.csv", show_col_types = F)
 ParameterTable <- readr::read_csv("../data/grambank/cldf/parameters.csv", show_col_types = F)
 
+#for computing the metric related to word order, it is necessary ot binarise the multistate features. This is done inside make_theo_scores, via other functions. We load all necessary functions here.
 source("../utility/rgrambank/make_binary_ParameterTable.R")
 source("../utility/rgrambank/make_binary_ValueTable.R")
 source("../utility/rgrambank/make_theo_scores.R")
 source("../utility/rgrambank/reduce_ValueTable_to_unique_glottocodes.R")
 
-ValueTable_binary <- make_binary_ValueTable(ValueTable = ValueTable, keep_multistate = FALSE, keep_raw_binary = TRUE)
-ParameterTable_binary <- make_binary_ParameterTable(ParameterTable = ParameterTable,
-                                                               keep_multi_state_features = FALSE,
-                                                               keep_raw_binary = TRUE)
-
-
 set.seed(72000)
 
-ValueTable_binary_reduced <- reduce_ValueTable_to_unique_glottocodes(ValueTable = ValueTable_binary, LanguageTable = LanguageTable, merge_dialects = TRUE, method = "combine_random") %>% 
+ValueTable_reduced <- reduce_ValueTable_to_unique_glottocodes(ValueTable = ValueTable, LanguageTable = LanguageTable, merge_dialects = TRUE, method = "combine_random") %>% 
   dplyr::select(-Language_ID) %>% 
   dplyr::rename(Language_ID = Glottocode)
 
-theo_scores <- make_theo_scores(ValueTable = ValueTable_binary_reduced, 
+theo_scores <- make_theo_scores(ValueTable = ValueTable_reduced, 
                                            ParameterTable = ParameterTable, Fusion_option = "count_one_and_half")
 
 theo_scores %>% 
