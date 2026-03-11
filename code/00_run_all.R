4fa95c4aad322939e13e8cee4661888c1f2940b3
HedvigS <hedvig.skirgard@gmail.com>
2026-03-11 21:08:39 +0100

    Update 00_run_all.R

--- code/00_run_all.R
+++ code/00_run_all.R
@@ -60,6 +60,7 @@ source("04_combine_all_data.R")
 source("04_plot_SPLOM.R")
 source("04_maps.R")
 source("04_UD_Genre_plot.R")
+source("05_rank_heat_plot.R")
 
 # Python post-processing
 source("05_box_plots.R") # Box plots showing the range of values across our metrics and MFH
